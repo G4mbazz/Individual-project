41f26bde01ae38baf8a8ed3d6ca9885d53e2434e
GambAss <GambAss@DESKTOP-UT6L55H>
2022-09-21 10:23:46 +0200

    Add project files.

---
+++ Individual project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Individual_project
+{
+    internal class Program
+    {
+        public static void Login()
+        {
+            int counter = 0;
+            while (counter < 3)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[,] user = new string[5,2] {{"admin", "3695"},{"sebastian","1569"},{"casper", "5170"},{"johan", "5784"},{"kalle", "3459"}};
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
