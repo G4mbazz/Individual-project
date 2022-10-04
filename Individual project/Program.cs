d81823b86aafb24ebf2af4aa7c8297960e600f12
G4mbazz <G4mbazz@hotmail.com>
2022-10-04 09:27:59 +0200

    WorkingOnLogin

--- Individual project/Program.cs
+++ Individual project/Program.cs
@@ -4,19 +4,42 @@ namespace Individual_project
 {
     internal class Program
     {
-        public static void Login()
+       
+        
+        public static void Login(string[,] user)
         {
             int counter = 0;
             while (counter < 3)
             {
+                Console.Clear();
+                Console.Write("Skriv in användarnamn: ");
+                string name = Console.ReadLine();
+                Console.Write("Skriv in pinkod: ");
+                string pin = Console.ReadLine();
+                foreach(string s in user)
+                {
+                    Console.WriteLine(s);
+                    Console.ReadLine();
 
+                }
+                
+                counter++;
             }
         }
         static void Main(string[] args)
         {
-            string[,] user = new string[5,2] {{"admin", "3695"},{"sebastian","1569"},{"casper", "5170"},{"johan", "5784"},{"kalle", "3459"}};
+            int[,] account = new int[5, 2] { { 5000, 21423 }, { 1000, 7245 }, { 500, 754 }, { 487, 15000 }, { 123, 52079 } };
+            string[,] user = new string[5, 2] { { "andreas", "3695" }, { "sebastian", "1569" }, { "casper", "5170" }, { "johan", "5784" }, { "kalle", "3459" } };
+            
+            
 
-            Console.WriteLine("Hello World!");
+            bool loop = true;
+
+            while (loop)
+            {
+                Login(user);
+            }
+
         }
     }
 }
