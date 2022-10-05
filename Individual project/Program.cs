e43256f08b48d8469c97abf45efa2c33ba1ed1a2
G4mbazz <G4mbazz@hotmail.com>
2022-10-05 08:56:18 +0200

    Login done, started work on menu

--- Individual project/Program.cs
+++ Individual project/Program.cs
@@ -4,25 +4,57 @@ namespace Individual_project
 {
     internal class Program
     {
-       
+       public static void MainMenu(string user, int[,] account, int i)
+       {
+            bool loop = true;
+            while (loop)
+            {
+                Console.Clear();
+                Console.WriteLine("Välkommen {0} Vänligen välj ett menyval", user);
+                Console.Write("\n\t1: Ta ut pengar\n\t2: Överförning\n\t4:Logga ut ");
+                Int32.TryParse(Console.ReadLine(), out int input);
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine($"Vilket konto vill ta ut ifrån?\n\t1: Bankkonto {account[i, 0]}\n\t2: Sparkonto {account[i, 1]}");
+
+
+                        break;
+
+
+
+
+                    case 4:
+                        loop = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+       }
         
-        public static void Login(string[,] user)
+        public static void Login(string[,] user, int[,] account)
         {
             int counter = 0;
             while (counter < 3)
             {
                 Console.Clear();
                 Console.Write("Skriv in användarnamn: ");
-                string name = Console.ReadLine();
+                string name = Console.ReadLine().ToLower();
                 Console.Write("Skriv in pinkod: ");
-                string pin = Console.ReadLine();
-                foreach(string s in user)
+                string pin = Console.ReadLine().ToLower();
+                for (int i = 0; i < user.Length; i++)
                 {
-                    Console.WriteLine(s);
-                    Console.ReadLine();
+                   if (user[i,0] == name && user[i,1] == pin)
+                   {
+                        MainMenu(name, account, i);
+                        counter = 3;
+                        break;
+                   }
 
                 }
-                
+                Console.Write("xd");
+                Console.ReadKey();
                 counter++;
             }
         }
@@ -37,7 +69,7 @@ namespace Individual_project
 
             while (loop)
             {
-                Login(user);
+                Login(user, account);
             }
 
         }
